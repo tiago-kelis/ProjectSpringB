ac2ccff997c063180edd32481996e2e26d27e61e
Tiago kelis <tiagokelis2017@gmail.com>
2024-07-21 19:51:24 -0300

    created table Order

--- src/main/java/com/DEvTk/course/Entities/User.java
+++ src/main/java/com/DEvTk/course/Entities/User.java
@@ -1,12 +1,15 @@
 package com.DEvTk.course.Entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,10 +26,12 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 		
 	}
-	
 	
 	
 
@@ -90,6 +95,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	
 	
 	@Override
@@ -109,6 +118,8 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return id == other.id;
 	}
+
+	
 
 	
 	
